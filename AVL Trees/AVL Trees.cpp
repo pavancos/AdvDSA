ac109e6cfab34deb2b014074716801a52eef9351
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-02-03 12:26:11 +0530

    add:link

---
+++ AVL Trees/AVL Trees.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int x,int l,int r){
+        data=x;
+        left=NULL;
+        right=NULL;
+    }
+    Node* initBST(Node *head, int data){
+        if()
+        
+    }
+
+};
+// typedef struct node{
+//     int data, left_index, right_index;
+// }Node;
+
+int main(){
+
+}
