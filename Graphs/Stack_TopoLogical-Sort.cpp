3dbb12fa5f6ad358f7d9c32c16500e73274f69b6
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-16 06:25:07 +0000

    refix

--- Graphs/Stack_TopoLogical-Sort.cpp
+++ Graphs/Stack_TopoLogical-Sort.cpp
@@ -1,26 +1,34 @@
+// A C++ program to print topological sorting of a DAG
 #include <iostream>
 #include <list>
 #include <stack>
 using namespace std;
-class Graph{
+class Graph
+{
 	int V;
 	list<int> *adj;
-	void topologicalSortUtil(int v, bool visited[], stack<int> &Stack){
+	void topologicalSortUtil(int v, bool visited[], stack<int> &Stack)
+	{
 		visited[v] = true;
 		list<int>::iterator i;
 		for (i = adj[v].begin(); i != adj[v].end(); ++i)
-			if (!visited[*i])	topologicalSortUtil(*i, visited, Stack);
+			if (!visited[*i])
+				topologicalSortUtil(*i, visited, Stack);
 		Stack.push(v);
 	}
-   public:
-	Graph(int V){
+
+public:
+	Graph(int V)
+	{
 		this->V = V;
 		adj = new list<int>[V];
 	}
-	void addEdge(int v, int w){
+	void addEdge(int v, int w)
+	{
 		adj[v].push_back(w);
 	}
-	void topologicalSort(){
+	void topologicalSort()
+	{
 		stack<int> Stack;
 		bool *visited = new bool[V];
 		for (int i = 0; i < V; i++)
@@ -33,17 +41,18 @@ class Graph{
 			Stack.pop();
 		}
 		cout<<endl;
+		
 	}
 };
 int main(){
-	Graph G(6);
-	G.addEdge(55, 27);
-	G.addEdge(55, 10);
-	G.addEdge(40, 10);
-	G.addEdge(40, 1);
-	G.addEdge(27, 37);
-	G.addEdge(37, 1);
+	Graph g(6);
+	g.addEdge(5, 2);
+	g.addEdge(5, 0);
+	g.addEdge(4, 0);
+	g.addEdge(4, 1);
+	g.addEdge(2, 3);
+	g.addEdge(3, 1);
 	cout << "Topological Sort: ";
-	G.topologicalSort();
+	g.topologicalSort();
 	return 0;
 }
