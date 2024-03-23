40e18b38f15de35a1c9f78cef7f46e2ca20c3b4a
Pavan Kumar Chennupati <pavankc005@gmail.com>
2024-03-23 06:45:15 +0000

    fix

--- AllPairShortestPath/Warshalls.cpp
+++ AllPairShortestPath/Warshalls.cpp
@@ -39,7 +39,7 @@ int main(){
     for(int i=0;i<V*V;i++){
         int s,d,w;
         cin>>s>>d>>w;
-        addEdge(graph,s,d,w);
+        addEdge(graph,s-1,d-1,w);
     }
     print(graph);
     cout<<"\nAll Pair Shortest Path:\n";
